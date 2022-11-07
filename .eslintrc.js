908e437d7f54da20f580a298315c22077f266af5
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-11-07 11:33:25 -0500

    ci: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,49 +3,28 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-    'prettier/recommended'
-  ],
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts']
-    },
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        project: './tsconfig.json'
-      }
-    }
-  },
+  plugins: [],
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': ['error'],
-    'semi': ['error', 'never'],
     'arrow-parens': ['error', 'as-needed'],
     'no-trailing-spaces': 'error',
     'import/extensions': 'off',
     'max-len': [
       'error',
       {
-        'code': 100,
-        'tabWidth': 2,
-        'ignoreComments': true,
-        'ignoreStrings': true
-      }
+        code: 100,
+        tabWidth: 2,
+        ignoreComments: true,
+        ignoreStrings: true,
+      },
     ],
-    'import/no-extraneous-dependencies': ["error", { "devDependencies": true }],
-    "@typescript-eslint/no-explicit-any": "off"
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 };
